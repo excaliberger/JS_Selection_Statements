3ac4e8e1edef3205f07ca7644690d1b87339793a
Emily Berger <excaliberger@gmail.com>
2022-10-27 17:16:15 -0400

    Exercise 1

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
 console.log("Hello World!\n==========\n");
+
+let magicNumber = 6;
+
+let guessMagicNumber = prompt("what's the magic number?")
+
+if (guessMagicNumber > magicNumber) {
+  console.log("too high!");
+} else if (guessMagicNumber < magicNumber) {
+  console.log("too low!");
+} else {
+  console.log("congratulations!!")
+}
+
 console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
